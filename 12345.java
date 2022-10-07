19553165d0bc98762c50abc280587fbb4bd36d67
Asiagodtone9527 <114204230+Asiagodtone9527@users.noreply.github.com>
2022-10-07 09:18:11 +0800

    Add files via upload

---
+++ 12345.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class 12345{
+
+      public static void main(String[] args){
+		  int total = 0;
+		   int gradeCounter = 0;
+		    int aCount = 0;
+			 int bCount = 0;
+			  int cCount = 0;
+			   int dCount = 0;
+			    int eCount = 0;
+		  
+		  System.out.println("Enter first integer:");
+			  int grade;
+			  	Scanner input = new Scanner(System.in);
+			  grade = input.nestInt();	
+			  
+		  Switch (grade / 10){
+			case 9 :
+			System.out.println("A");
+			break;
+		  }
+	
+}
+}
