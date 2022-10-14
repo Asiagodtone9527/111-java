fa39264d8ff1c9b6e785df90af120beaeac346e1
Asiagodtone9527 <114204230+Asiagodtone9527@users.noreply.github.com>
2022-10-14 09:38:09 +0800

    Add files via upload

---
+++ Test10.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Test10 {
+    public static void main(String[] args){
+		int row = 0;
+		int row2 = 0;
+		String space = "*";
+		
+		while (row < 5) {
+			int star = 0;
+			while (star <= row){
+				System.out.printf("*");
+				star = star + 1;
+			}
+			System.out.println("");
+			row = row + 1;
+		}
+		while (row2 < 5 ) {
+			int star2 = 4;
+			while (star2 > row2){
+				System.out.printf("*");
+				star2 = star2 - 1;
+			}
+			System.out.println("");
+			row2 = row2 + 1;
+		}
+	}
+}
