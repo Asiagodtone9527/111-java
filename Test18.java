0c4ec532c79f36e08fe512574368ee681291ce90
Asiagodtone9527 <114204230+Asiagodtone9527@users.noreply.github.com>
2022-11-25 10:00:31 +0800

    Add files via upload

---
+++ Test18.java
@@ -0,0 +1,19 @@
+public class Test18 {
+    public static void main(String[] args){
+        int width=0;
+		for(int i= -4; i<=4;i++){
+			for(int k = -5; k <=5;k++){
+				if(Math.abs(k) > width)
+					System.out.printf("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println("");
+			if(i < 0)
+				width++;
+			else
+				width--;
+			}
+		}
+	}
+
