fb64c32cbd8a7c50bad7e3a445e9bef2694b5603
Asiagodtone9527 <114204230+Asiagodtone9527@users.noreply.github.com>
2022-11-25 11:25:40 +0800

    Add files via upload

---
+++ Test19.java
@@ -0,0 +1,19 @@
+import java.net.*;
+import java.util.*;
+public class Test18 {
+    public static void main(String[] args){
+		int k;
+		Scanner input = new Scanner(System.in);
+		System.out.println("km");
+		k = input.nextInt();
+			if (k > 7){
+				System.out.println((85 + (k - 7) * 5)+"NT");	
+			}		
+			else{
+				System.out.println("85NT");	
+			}
+
+			}
+		}
+	
+
